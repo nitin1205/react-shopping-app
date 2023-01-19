cd4b9829259c06883ae95675ab46bc04d7389ed8
Nitin Singh <vortechcorp@gmail.com>
2023-01-19 19:15:58 +0530

    completed user signup and signin feature

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+import { 
+    getAuth,
+    signInWithPopup, 
+    GoogleAuthProvider, 
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword 
+} from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 
@@ -61,7 +67,11 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
     return await createUserWithEmailAndPassword(auth, email, password);
 };
 
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
 
+    return await signInWithEmailAndPassword(auth, email, password);
+}
 
 
 
