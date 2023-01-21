e31ee28fb35af13dd53dfe858e28af0f8b7c98ae
Nitin Singh <vortechcorp@gmail.com>
2023-01-21 15:52:27 +0530

    refactored the code

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -8,6 +8,12 @@ import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+// import {
+//     NavigationContainer,
+//     LogoContainer,
+//     NavLinks,
+//     NavLink,
+//   } from './navigation.styles';
 
 const Navigation = () => {
 
@@ -37,9 +43,27 @@ const Navigation = () => {
                 </div>
                 {isCartOpen && <CartDropdown />}
             </div>
+            {/* <NavigationContainer>
+                <LogoContainer to='/'>
+                    <Logo />
+                </LogoContainer>
+                <NavLinks>
+                    <NavLink to='/shop'>SHOP</NavLink>
+
+                    {currentUser ? (
+                    <NavLink as='span' onClick={signOutUser}>
+                        SIGN OUT
+                    </NavLink>
+                    ) : (
+                    <NavLink to='/auth'>SIGN IN</NavLink>
+                    )}
+                    <CartIcon />
+                </NavLinks>
+                {isCartOpen && <CartDropdown />}
+            </NavigationContainer> */}
             <Outlet />
         </Fragment>
-    )
-}
+    );
+};
 
 export default Navigation;
