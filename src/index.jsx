6e8af73c152ef716d0bb3a12a9bacfb96f6b4adf
Nitin Singh <vortechcorp@gmail.com>
2023-01-22 23:40:58 +0530

    persisted user cart data using redux-persist

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.scss';
 import App from './App';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 // import { UserProvider } from './contexts/user.context';
 // import { CategoriesProvider } from './contexts/categories.context';
 // import { CartProvider } from './contexts/cart.context';
@@ -15,15 +16,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store} >
-      <BrowserRouter >
-        {/* <UserProvider> */}
-          {/* <CategoriesProvider> */}
-            {/* <CartProvider> */}
-              <App />
-            {/* </CartProvider> */}
-          {/* </CategoriesProvider> */}
-        {/* </UserProvider> */}
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          {/* <UserProvider> */}
+            {/* <CategoriesProvider> */}
+              {/* <CartProvider> */}
+                <App />
+              {/* </CartProvider> */}
+            {/* </CategoriesProvider> */}
+          {/* </UserProvider> */}
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
